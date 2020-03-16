e07549963f9fd5c5e257b1e1f99006fdb46f9cb0
valliappan22 <valliappan445@gmail.com>
2020-03-17 00:48:38 +0530

    initalconversation

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from mycroft.audio import wait_while_speaking
 
 class Meetingroom(MycroftSkill):
     def __init__(self):
@@ -7,9 +7,28 @@ class Meetingroom(MycroftSkill):
 
     @intent_file_handler('meetingroom.intent')
     def handle_meetingroom(self, message):
-        self.speak_dialog('meetingroom')
+        meeting_type = message.data.get('roomName')
+        if meeting_type is not None:
+            self.speak_dialog("i don't know about the",
+                              {'type': meeting_type})
+            self.doyouwantToRepeat()
+        else:
+            self.speak_dialog('meetingroom')
+            response = self.get_response('meetingroom', num_retries=0)
+    @intent_file_handler('repeat.intent')
+    def doyouwantToRepeat():
+         self.speak_dialog('Do you want me to repeat')
+         response = self.get_response('repeat', num_retries=0)
+         if response is None:
+            self.speak_dialog('Have a nice day')
+        if response is not None:
+            self.speak_dialog('Have a nice day')
+
+    def stop(self):
+    pass
+
+        
 
 
 def create_skill():
     return Meetingroom()
-
